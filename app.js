96c1253413575357c982706426483c529c2bb3d3
Josias Alves <81787907+josi-dev@users.noreply.github.com>
2025-11-01 10:39:04 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+let divOpcoes = [...document.querySelectorAll("#opcoes .box-opcoes")]
+
+
+divOpcoes[0].addEventListener("click", ()=>{setTimeout(()=>{location.href='./exemple.html'},400)})
+
+
+divOpcoes[1].addEventListener("click", ()=>{setTimeout(()=>{location.href='./exemple.html'},400)})
+
+
+divOpcoes[2].addEventListener("click", ()=>{setTimeout(()=>{location.href='./exemple.html'},400)})
+
+
+divOpcoes[3].addEventListener("click", ()=>{setTimeout(()=>{location.href='./exemple.html'},400)})
+
+
+
+
+
+/**/
